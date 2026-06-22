de176f64da986bd89072d8dac6252a2794a2e4b9
BagoesPantera <bagoespantera01@gmail.com>
2026-06-22 11:20:00 +0800

    feat: add contact page

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
     <p>Name Brand</p>
     <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
+    <router-link to="/contact">Contact</router-link>
   </nav>
-  
+
   <router-view />
 </template>
